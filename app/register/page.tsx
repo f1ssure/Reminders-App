957da996325606d6bb8821eafe82ca3ed2b2d214
Gabrielius Bakas <gabrielius.bakas@mif.stud.vu.lt>
2025-11-09 20:00:18 +0200

    change visuals of /register page, tweak InputField component

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,16 +3,17 @@ import InputField from "@components/InputField"
 export default function RegisterPage() {
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center justify-evenly w-full max-w-150 h-150 p-8">
+      <div className="flex flex-col items-center justify-evenly w-full max-w-125 gap-10 p-8">
         <div>
           <h1 className="text-center text-4xl font-bold">
             Reminders App
           </h1>
           <p className="text-center text-xs mt-2">Never Forget Anything.</p>
         </div>
-        
-        <InputField label="Email" type="text" placeholder="Enter your email address" />
-        <InputField label="Password" type="password" placeholder="Create a password" />
+        <div className="flex flex-col gap-5 w-full">
+          <InputField label="Email" type="text" placeholder="Enter your email address" />
+          <InputField label="Password" type="password" placeholder="Create a password" />
+        </div>
       </div>
     </main>
   );
--- components/InputField/index.tsx
+++ components/InputField/index.tsx
@@ -6,10 +6,10 @@ interface InputProps {
 
 export default function InputField(props: InputProps) {
   return (
-    <label>
+    <label className="w-full">
       {props.label}
       <input 
-        className="block"
+        className="block w-full mt-1 px-4 py-2 border-zinc-800 border-[1.5px] transition-outline ease-out duration-75 focus:outline-solid outline-sky-950 focus:outline-1 rounded-md"
         type={props.type ? props.type : "text"}
         placeholder={props.placeholder}
       ></input>
